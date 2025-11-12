f926009d7efd15a6077df8890bee24acf95f3dae
Diegodilasa <diegodilasa@gmail.com>
2025-11-11 22:19:13 -0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -256,7 +256,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["https://app7d.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
